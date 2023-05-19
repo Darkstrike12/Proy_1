e590b6a4703223e2cf91bb06fdbcd220f8bd31c5
ThonyGtz <anthony.salazar6789@alumnos.udg.mx>
2023-05-18 23:18:13 -0600

    Enemigos

---
+++ Assets/Scripts/SlimeBasic_Movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeBasic_Movement : MonoBehaviour
+{
+    public GameObject[] positioner;
+    private int currentPositionIndex = 0;
+    public float speed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(positioner[currentPositionIndex].transform.position, transform.position) < .1f)
+        {
+            currentPositionIndex++;
+            if (currentPositionIndex >= positioner.Length)
+            {
+                currentPositionIndex = 0;
+            }
+           flip();
+        }
+        transform.position = Vector2.MoveTowards(transform.position, positioner[currentPositionIndex].transform.position, Time.deltaTime * speed);
+    }
+    
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
